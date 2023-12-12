9eb47896aab8cf1a79ae8a8550b84602f1e1212c
karthi354 <116268464+karthi354@users.noreply.github.com>
2023-12-12 11:55:41 +0530

    assessment 2 completed

---
+++ demo_site (2)/demo_site/js/assessment_script.js
@@ -0,0 +1,6 @@
+const element = document.getElementById("placeOrder");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "comming";
+}
